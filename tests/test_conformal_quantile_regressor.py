1cf1c1d7c3e59d879aa2ada1ea512bf4b2db27a4
Laurent Sorber <laurent.sorber@gmail.com>
2024-06-11 16:34:35 +0000

    feat: improve sample efficiency

--- tests/test_conformal_quantile_regressor.py
+++ tests/test_conformal_quantile_regressor.py
@@ -1,6 +1,7 @@
 """Test the Conformal Coherent Quantile Regressor."""
 
 import numpy as np
+import pytest
 from sklearn.base import BaseEstimator
 from sklearn.utils.estimator_checks import check_estimator
 from xgboost import XGBRegressor
@@ -9,13 +10,25 @@ from conformal_tights import ConformalCoherentQuantileRegressor
 from tests.conftest import Dataset
 
 
-def test_conformal_quantile_regressor_coverage(dataset: Dataset, regressor: BaseEstimator) -> None:
+@pytest.mark.parametrize("prefit", [True, False], ids=["prefit=True", "prefit=False"])
+def test_conformal_quantile_regressor_coverage(
+    dataset: Dataset,
+    regressor: BaseEstimator,
+    prefit: bool,  # noqa: FBT001
+) -> None:
     """Test ConformalCoherentQuantileRegressor's coverage."""
     # Unpack the dataset.
     X_train, X_test, y_train, y_test = dataset
     # Train the models.
+    if prefit and isinstance(regressor, BaseEstimator):
+        if isinstance(regressor, XGBRegressor):
+            regressor.set_params(enable_categorical=True)
+        regressor.fit(X_train, y_train)
     model = ConformalCoherentQuantileRegressor(estimator=regressor)
     model.fit(X_train, y_train)
+    # Verify that the prefitted model was used.
+    if prefit and isinstance(regressor, BaseEstimator):
+        np.testing.assert_array_equal(model.predict(X_test), regressor.predict(X_test), strict=True)
     # Verify the coherence of the predicted quantiles.
     ŷ_quantiles = model.predict(X_test, quantiles=np.linspace(0.1, 0.9, 3))
     for j in range(ŷ_quantiles.shape[1] - 1):
